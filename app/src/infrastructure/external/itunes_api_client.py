e87674e46b11cff929d7212afc6bb17c9d4776bc
Kondo Daisuke <gim0kondo@gmail.com>
2021-10-28 12:58:54 +0900

    :sparkles: driver for iTunes API

---
+++ app/src/infrastructure/external/itunes_api_client.py
@@ -0,0 +1,13 @@
+import json
+from typing import Any, Dict
+
+import requests
+
+from ...adapter.external.api_client import ApiClient
+
+ITUNES_SEARCH_API_URL = "https://itunes.apple.com/search"
+
+class ITunesApiClient(ApiClient):
+    def search(self, params: Dict[str, str]) -> Dict[str, Any]:
+        res = requests.get(ITUNES_SEARCH_API_URL, params=params)
+        return json.load(res.text)
